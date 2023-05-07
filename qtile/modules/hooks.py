bf837a0fe2e09bd4e9ded2a549f797111ad18292
vinsaliv <117625932+vinsaliv@users.noreply.github.com>
2023-05-07 14:30:35 +0800

    Add Qtile

---
+++ qtile/modules/hooks.py
@@ -0,0 +1,8 @@
+import os
+import subprocess
+from libqtile import hook
+
+@hook.subscribe.startup
+def autostart():
+    home = os.path.expanduser("~/.config/qtile/autostart.sh")
+    subprocess.call([home])
